b88b014fe359de0f700a2049b65e3862fc138bbe
Livia Corsetti <tavo-github-email@example.com>
2025-11-14 15:56:46 +0100

    feat(admin): pridėtas konfigūracijos puslapis (enable + quality)

--- moocfimage.php
+++ moocfimage.php
@@ -14,7 +14,7 @@ class Moocfimage extends Module
         $this->author = 'moonia';
         $this->need_instance = 0;
         $this->ps_versions_compliancy = [
-            'min' => '9.0.0.0',
+            'min' => '9.0.0',
             'max' => _PS_VERSION_,
         ];
         $this->bootstrap = true;
@@ -40,5 +40,94 @@ class Moocfimage extends Module
         Configuration::deleteByName('MOOCFIMAGE_QUALITY');
 
         return parent::uninstall();
+    }
+
+    public function getContent()
+    {
+        $output = '';
+
+        if (Tools::isSubmit('submitMoocfimage')) {
+            $enabled = (int) Tools::getValue('MOOCFIMAGE_ENABLED', 1);
+            $quality = (int) Tools::getValue('MOOCFIMAGE_QUALITY', 85);
+
+            if ($quality < 1 || $quality > 100) {
+                $output .= $this->displayError($this->l('Quality turi būti tarp 1 ir 100.'));
+            } else {
+                Configuration::updateValue('MOOCFIMAGE_ENABLED', $enabled);
+                Configuration::updateValue('MOOCFIMAGE_QUALITY', $quality);
+                $output .= $this->displayConfirmation($this->l('Nustatymai atnaujinti.'));
+            }
+        }
+
+        return $output . $this->renderForm();
+    }
+
+    protected function renderForm()
+    {
+        $defaultLang = (int) Configuration::get('PS_LANG_DEFAULT');
+
+        $fieldsForm = [
+            'form' => [
+                'legend' => [
+                    'title' => $this->l('Cloudflare Images nustatymai'),
+                    'icon' => 'icon-picture',
+                ],
+                'input' => [
+                    [
+                        'type' => 'switch',
+                        'label' => $this->l('Įjungta'),
+                        'name' => 'MOOCFIMAGE_ENABLED',
+                        'is_bool' => true,
+                        'values' => [
+                            [
+                                'id' => 'active_on',
+                                'value' => 1,
+                                'label' => $this->l('Taip'),
+                            ],
+                            [
+                                'id' => 'active_off',
+                                'value' => 0,
+                                'label' => $this->l('Ne'),
+                            ],
+                        ],
+                        'desc' => $this->l('Išjungus, grįžtama prie originalių paveikslėlių URL.'),
+                    ],
+                    [
+                        'type' => 'text',
+                        'label' => $this->l('Quality (1–100)'),
+                        'name' => 'MOOCFIMAGE_QUALITY',
+                        'class' => 'fixed-width-sm',
+                        'suffix' => '%',
+                        'desc' => $this->l('Cloudflare quality parametras. Rekomenduojama 75–90. Numatyta 85.'),
+                    ],
+                ],
+                'submit' => [
+                    'title' => $this->l('Išsaugoti'),
+                ],
+            ],
+        ];
+
+        $helper = new HelperForm();
+        $helper->module = $this;
+        $helper->name_controller = $this->name;
+        $helper->identifier = $this->identifier;
+        $helper->token = Tools::getAdminTokenLite('AdminModules');
+        $helper->currentIndex = AdminController::$currentIndex . '&configure=' . $this->name;
+        $helper->default_form_language = $defaultLang;
+        $helper->allow_employee_form_lang = $defaultLang;
+        $helper->title = $this->displayName;
+        $helper->show_toolbar = false;
+        $helper->submit_action = 'submitMoocfimage';
+        $helper->fields_value = $this->getConfigFormValues();
+
+        return $helper->generateForm([$fieldsForm]);
+    }
+
+    protected function getConfigFormValues()
+    {
+        return [
+            'MOOCFIMAGE_ENABLED' => (int) Configuration::get('MOOCFIMAGE_ENABLED', 1),
+            'MOOCFIMAGE_QUALITY' => (int) Configuration::get('MOOCFIMAGE_QUALITY', 85),
+        ];
     }
 }
